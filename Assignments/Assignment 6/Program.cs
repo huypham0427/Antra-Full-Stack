bca5f1fdea081e8d375d582d6dbf7227fcef194e
huypham0427 <59068418+huypham0427@users.noreply.github.com>
2023-03-29 16:29:15 -0700

    Add files via upload

---
+++ Assignments/Assignment 6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class LeetCode {
+  static void Main() {
+    Console.WriteLine("LeetCode-2");
+  }
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        var reslist = new List<IList<int>>();
+            Array.Sort(nums);
+            var pre = 0; 
+                for(var i = nums.Length-1; i>1; i--)
+                {
+                    //if not at the start of loop and previous number equals to current number
+                    //no need to continue, the triplet (if there is one) will be the same
+                    if(i != nums.Length-1 && nums[i] == pre){
+                        continue;
+                    }
+                    //calculate how many we still need to compose the triplet
+                    var remain = 0 - nums[i];
+                    //update the previous number
+                    pre = nums[i];
+                    //the previous tracking for inner loop
+                    var prev = 0;
+                    for(var j = i-1; j>0; j--){
+                        //same as outer loop, if number is same
+                        //would yield same triplet, skip it.
+                        if(j != i-1 && nums[j] == prev){
+                            continue;
+                        }
+                        //calculate the last number needed
+                        var last = remain - nums[j];
+                        //update the previous
+                        prev = nums[j];
+                        //binary search the last number
+                        var exist = Array.BinarySearch<int>(nums, 0, j, last);
+                        //if found add the triplet
+                        if(exist >= 0)
+                        {
+                            reslist.Add(new List<int>(){nums[i], nums[j], last});
+                        }
+                    }
+                }
+        return reslist;
+    }
+
+}
